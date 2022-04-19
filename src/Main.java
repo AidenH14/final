528e7eae52de27e52f050d77aab4be38f9473be7
AidenH14 <aiden.hughes@cecstudents.org>
2022-04-19 09:38:11 -0600

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+/* Aiden Hughes
+April 18, 2022
+Final Project
+Purpose:
+    Create a workout generator depending on the person.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Workout workout[] = new Workout[12];
+
+
+    }
+
+
+
+
+}
